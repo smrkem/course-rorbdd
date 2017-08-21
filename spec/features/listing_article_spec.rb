7232085117d7bb6e1f6e662e5c350e73b9a7c2a7
matt-smrke <matthew.smrke@empire.ca>
2017-08-21 16:43:45 -0400

    adds no articles message

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -16,4 +16,16 @@ RSpec.feature "Listing Articles" do
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
   end
+
+  scenario "A user has no articles" do
+    Article.delete_all
+    visit "/"
+
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_content(@article2.title)
+
+    within ("h1#no-articles") do
+      expect(page).to have_content("There are no articles.")
+    end
+  end
 end
