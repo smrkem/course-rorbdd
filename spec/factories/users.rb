18319a0fb574ade96477c56608d9cbf0917f05ca
matt-smrke <matthew.smrke@empire.ca>
2017-08-22 13:57:03 -0400

    adds user signin

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    
+    email "mail@example.com"
+    password "testpassword"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:user)).to be_valid
+  end
+
+  it "is invalid without an email" do
+    user = FactoryGirl.build(:user, email: "")
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid without a password" do
+    user = FactoryGirl.build(:user, password: "")
+    expect(user).not_to be_valid
+  end
 end
